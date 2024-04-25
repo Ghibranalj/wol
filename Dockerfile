3f15914f5f09a175205d337498efc460d2673e98
Ghibranalj <24712554+Ghibranalj@users.noreply.github.com>
2024-04-25 18:10:11 +0700

    feat: add dockerfile and script to build docker cross platform

---
+++ Dockerfile
@@ -0,0 +1,19 @@
+# Use an official Ubuntu base image that supports multiple architectures
+FROM ubuntu:latest
+
+# Set the working directory
+WORKDIR /app
+
+# Install GCC for compiling C programs
+RUN apt-get update && apt-get install -y gcc
+
+# Copy the C source file into the Docker image
+COPY main.c /app
+
+# Compile the C program to a binary named 'program'
+RUN gcc main.c -o wol
+
+ENV MAC_ADDRESS="a8:a1:59:e8:b3:16"
+ENV LISTEN_PORT="5431"
+# Command to run the compiled binary
+CMD ["sh", "-c", "./program $MAC_ADDRESS $LISTEN_PORT"]
---
+++ buildx.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env sh
+
+
+ACTION=$1
+
+if [ "$ACTION" = "setup" ]; then
+    docker buildx create --name wol-builder --use --driver docker-container
+    docker buildx use wol-builder
+    docker buildx inspect --bootstrap
+    exit 0
+fi
+
+if [ "$ACTION" = "build" ]; then
+    docker buildx build --platform linux/amd64,linux/arm64,linux/arm/v7 -t wol:latest .
+    exit 0
+fi
+
+if [ "$ACTION" = "push" ]; then
+    printf "Enter your dockerhub username: "
+    read DOCKER_USERNAME
+    printf "Enter your docker image name: "
+    read DOCKER_IMAGE_NAME
+    docker buildx build --platform linux/amd64,linux/arm64,linux/arm/v7 -t $DOCKER_USERNAME/$DOCKER_IMAGE_NAME:latest --push .
+    exit 0
+fi
+
+echo "Usage:" $0 "[setup|build|push]"
+    cat <<EOF
+            setup: setup docker buildx
+            build: build docker image
+            push: build and push docker image to dockerhub
+EOF
